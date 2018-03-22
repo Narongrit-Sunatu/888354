491745b58cc2245860014240ec4e09328d9cfc55
Narongrit Sunatu <58160657@go.buu.ac.th>
2018-03-22 21:29:51 +0700

    21/03/61

--- angular/my-first-app/src/app/app.module.ts
+++ angular/my-first-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './components/product/product.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: "", component: ProfileComponent },
+  { path: "products", component: ProductComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
